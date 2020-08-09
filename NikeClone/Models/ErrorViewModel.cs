f26058638042b92fc061d460ed690a3df1a1e9cd
Michael Keaveny <michaelkeaveny0@gmail.com>
2020-08-09 23:07:06 +0100

    Initial Commit

---
+++ NikeClone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NikeClone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
